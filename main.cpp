f1b3ffbdfc9fe45025803819c568f840a22f0fa0
d34dm4n <szilveszter.pinter@gmail.com>
2022-09-27 16:49:39 +0200

    No change implemented

--- main.cpp
+++ main.cpp
@@ -1,31 +1,4 @@
-<<<<<<< HEAD
-#include <iostream>
 
-constexpr int N_ELEMENTS = 100;
-
-int main()
-{
-    int *b = new int[N_ELEMENTS];
-    std::cout <<"1-100 ertekek duplazasa"<<std::endl;
-    for(int i = 0; i<=N_ELEMENTS; i++)
-    {
-        b[i] = i * 2;
-    }
-    for (int i = 0; i<=N_ELEMENTS; i++)
-    {
-        std::cout << "Ertek:"<<b[i]<<std::endl;
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i <= N_ELEMENTS; i++)
-    {
-        atlag += b[i];
-    }
-    atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
-    std::cout << "valami";
-    return 0;
-=======
 #include <iostream>
 
 constexpr int N_ELEMENTS = 100;
@@ -52,5 +25,4 @@ int main()
     std::cout << "Atlag: " << atlag << std::endl;
     std::cout << "Irok valamit." << std::endl;
     return 0;
->>>>>>> 7418164ec0138f0e5d2c0f657b46a83953c3ef3b
 }
